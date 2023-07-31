a884bcf78bf85721b9123e09908f53337596d053
txya900619 <wayne900619@gmail.com>
2023-07-31 16:03:20 +0000

    docs: add README.md and fix some help message

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # compute-ssl-feats
+A kaldi compute-mfcc-feats replacement with ssl model
+
+## install
+```bash
+git clone git@github.com:txya900619/compute_ssl_feats.git
+cd  compute_ssl_feats & pip install -e .
+```
+
+## Usage
+### options
+  - `-h, --help`  show this help message and exit
+  - `--model_name MODEL_NAME` Hugginface model name or model path
+  - `--layer LAYER`   Index of the layer of the output feature, or the last layer if not specified
+  - `--sampling_rate SAMPLING_RATE`
+
+## example
+output facebook/wav2vec2-xls-r-300m layer 24 features to test.ark
+```bash
+compute-ssl-feats --model_name="facebook/wav2vec2-xls-r-300m" --layer=24 "scp:wav.scp" "ark,scp:test.ark,test.scp"
+```
--- compute_ssl_feats/cli.py
+++ compute_ssl_feats/cli.py
@@ -7,9 +7,6 @@ from compute_ssl_feats.dump_feature import dump_feature
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--write_num_frames", type=str, help="Specify wspecifer for utt2num_frames"
-    )
-    parser.add_argument(
         "rspecifier", type=str, help="Read specifier for feats. e.g. ark:some.ark"
     )
     parser.add_argument(
@@ -19,7 +16,7 @@ def get_parser():
         "--model_name", type=str, help="Hugginface model name or model path"
     )
     parser.add_argument(
-        "--layer", type=int, help="Index of feature layer"
+        "--layer", type=int, help="Index of the layer of the output feature, or the last layer if not specified"
     )
     parser.add_argument("--sampling_rate", type=int, default=16_000)
 
@@ -36,6 +33,5 @@ def entrypoint():
         layer=args.layer,
         device="cuda",
         sampling_rate=args.sampling_rate,
-        # write_num_frames=args.write_num_frames,
     )
 
